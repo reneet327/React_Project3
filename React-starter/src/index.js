863ca8325c94acb4e5de444103c27848899e6765
reneet327 <reneet327@gmail.com>
2017-12-17 20:22:27 -0800

    updated samples from the chapter

--- React-starter/src/index.js
+++ React-starter/src/index.js
@@ -1,1 +1,17 @@
-alert("it's alive");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Connected extends React.Component {
+  render() {
+    return (
+      <div>
+        <Name firstName={this.props.firstName} lastName={this.props.lastName} /> is {this.props.status}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Connected firstName="Margaret" lastName="Hamilton" status="Connected" />
+  document.getElementById('root')
+);
