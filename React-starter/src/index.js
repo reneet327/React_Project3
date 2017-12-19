4f4c84a16bad16f2dc2265fc854bc427a8a33391
reneet327 <reneet327@gmail.com>
2017-12-18 19:42:39 -0800

    Final Version Project 3

--- React-starter/src/index.js
+++ React-starter/src/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Greeting extends React.Component{
-  componentWillMount(){
-    console.log("I'm 1st");
-  }
 
-  componentDidMount(){
-    console.log("I'm 2nd");
-  }
+
+class TodoList extends React.Component{
   render(){
+    const list = this.props.items.map(item => (<li key={item}>{item}</li>));
+
     return(
-      <h1>Hello World!</h1>
+
+      <div>
+        <h1>{this.props.title}</h1>
+          <ul>
+            {list}
+          </ul>
+      </div>
     )
   }
 }
 
-
 ReactDOM.render(
-<Greeting/>
+<TodoList title="Student Todo List" items={['Sign up for code review', 'Finish TodoList component', 'Get lots of sleep']}/>
   , document.getElementById('root')
 );
